c055ded07f8a7ed5ec02dda56d5a084d31e50f08
Dev-JeanSantos <jeancbsan@gmail.com>
2021-08-30 16:25:53 -0300

    primeiro commit

---
+++ src/main/java/com/example/teste_charles/TesteCharlesApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_charles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCharlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCharlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teste_charles/TesteCharlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste_charles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCharlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
